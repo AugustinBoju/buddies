5506aa3771523ac43776b4c3dee19de5b68e017f
manon chauvin <manonlz.chauvin@gmail.com>
2017-05-23 13:56:54 +0200

    seed creation

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,42 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database'
+Request.destroy_all
+Interest.destroy_all
+UserInterest.destroy_all
+User.destroy_all
+puts 'Cleaning database done'
+
+user1 = User.create!(email: "apolline@gmail.com", password: "totototo", first_name: "Apolline", last_name: "Sentucq")
+user2 = User.create!(email: "augustin@gmail.com", password: "totototo", first_name: "Augustin", last_name: "Boju")
+user3 = User.create!(email: "manon@gmail.com", password: "totototo", first_name: "Manon", last_name: "Chauvin")
+
+puts 'Creating interests...'
+interests = [
+  {
+    name: "Hiking"
+    },
+  {
+    name: "Surf"
+    },
+  {
+    name: "Nightlife"
+    },
+  {
+    name: "Yoga"
+    },
+  {
+    name: "Beach"
+    },
+  {
+    name: "Moutains"
+    },
+  {
+    name: "Photograpy"
+    },
+  {
+    name: "Natural Parks"
+    },
+]
+
+Interest.create!(interests)
+
+puts 'Seed finished'
