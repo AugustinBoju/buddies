3dda80a0ce2d23ac2bc27ae2cfae16d1a6ed2e39
manon chauvin <manonlz.chauvin@gmail.com>
2017-05-23 15:18:05 +0200

    routes resolution

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'pages#home'
-  resources :users, only [:index, :show] do
-    resources :requests, only [:index, :create]
+  root 'pages#home'
+  resources :users, only: [:index, :show] do
+    resources :requests, only: [:index, :create], shallow:true
   end
 end
