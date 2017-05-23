cb9a156593a05356f2fdb5fb22b60013a798dcf2
manon chauvin <manonlz.chauvin@gmail.com>
2017-05-23 14:25:13 +0200

    correction routers

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   resources :users, only [:index, :show] do
-    resources requests, only [:index, :create]
+    resources :requests, only [:index, :create]
   end
 end
