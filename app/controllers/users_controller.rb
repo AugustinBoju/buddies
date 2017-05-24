b71e3469cb1066023003ac76b646fb7429f95a13
Boju Augustin <bojuaugustin@gmail.com>
2017-05-24 14:45:17 +0200

    delete bybug

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,6 @@ class UsersController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :index ]
 
   def index
-    byebug
     selected_interest_ids = params[:search].select { |k, v| v == "true" }.keys.map(&:to_i)
     @users = User.where.not(id: current_user.id)
     if selected_interest_ids.empty?
