520bcbdf3be3b7a33429ca4c7c8fdd4daa11bbe7
Boju Augustin <bojuaugustin@gmail.com>
2017-05-26 16:12:42 +0200

    style for navbar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,9 @@
 module ApplicationHelper
+
+  def render_navbar
+    render 'shared/navbar' unless home
+  end
+
   def set_image_for_checkbox(text)
     case text.downcase
     when "hiking"
@@ -22,5 +27,11 @@ module ApplicationHelper
       "https://unsplash.it/200/300?image=#{rand(1..100)}"
     end
 
+  end
+
+  private
+
+  def home
+    controller_name == 'pages' && action_name == 'home'
   end
 end
