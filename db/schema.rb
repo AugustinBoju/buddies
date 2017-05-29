c4a7d58415147f8688260194a5ec847275500d16
manon chauvin <manonlz.chauvin@gmail.com>
2017-05-29 18:04:00 +0200

    CSS on user#index

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170525131624) do
+ActiveRecord::Schema.define(version: 20170529094707) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -60,6 +60,7 @@ ActiveRecord::Schema.define(version: 20170525131624) do
     t.float    "latitude"
     t.float    "longitude"
     t.string   "current_city"
+    t.string   "picture"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
