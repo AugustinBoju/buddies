cce105ddfcd3ff7874a442905ad5ba83e6338f56
apolline sentucq <apollinesentucq@gmail.com>
2017-05-30 14:44:17 +0200

    optimize pictures of interests

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,21 +7,21 @@ module ApplicationHelper
   def set_image_for_checkbox(text)
     case text.downcase
     when "hiking"
-      image_path 'hike.jpg'
+      image_path 'interests/hike.jpg'
     when "surf"
-      image_path 'surf.jpg'
+      image_path 'interests/surf.jpg'
     when "wild life"
-      image_path 'wildlife.jpg'
+      image_path 'interests/wildlife.jpg'
     when "yoga"
-      image_path 'yoga.jpg'
+      image_path 'interests/yoga.jpg'
     when "beach"
-      image_path 'beach.jpg'
+      image_path 'interests/beach.jpg'
     when "mountains"
-      image_path 'mountains.jpg'
+      image_path 'interests/mountains.jpg'
     when "photography"
-      image_path 'photo.jpg'
+      image_path 'interests/photo.jpg'
     when "natural park"
-      image_path 'naturalpark.jpg'
+      image_path 'interests/naturalpark.jpg'
 
     else
       "https://unsplash.it/200/300?image=#{rand(1..100)}"
